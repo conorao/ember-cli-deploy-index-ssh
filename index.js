b3febe1ce3f155edf31c95592f00a34513971e24
Conor Odell <conorao@users.noreply.github.com>
2015-11-12 17:16:50 -0500

    oops

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 /* jshint node: true */
 'use strict';
 
+var ssh2 = require('ssh2');
+var Promise = require('ember-cli/lib/ext/promise');
+
 var BasePlugin = require('ember-cli-deploy-plugin');
 
 module.exports = {
@@ -9,13 +12,31 @@ module.exports = {
   createDeployPlugin: function(options) {
     var DeployPlugin = BasePlugin.extend({
       name: options.name,
+
+      defaultConfig: {
+        destinationPort: 22,
+        dstHost: 'localhost',
+        privateKeyPath: '~/.ssh/id_rsa',
+      },
+
       requiredConfig: [
         'host',
         'port'
       ],
 
+      setup: function(context) {
+        return {
+          client: new ssh2.Client()
+        };
+      },
+
+      willDeploy: function(context) {
+        this.log(context.client);
+      }
+
       didBuild: function(context) {
         //do something amazing here once the project has been built
+        this.log('ARE YOU READY TO ROCK?' { color: 'blue' });
       },
 
       upload: function(context) {
