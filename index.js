fd06d39da05636d6d6654a62108d56d4de9eb776
Conor Odell <conorao@users.noreply.github.com>
2015-11-12 15:54:50 -0500

    wip scaffolding the initial plugin

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
 /* jshint node: true */
 'use strict';
 
+var BasePlugin = require('ember-cli-deploy-plugin');
+
 module.exports = {
   name: 'ember-cli-deploy-index-ssh'
+
+  createDeployPlugin: function(options) {
+    var DeployPlugin = BasePlugin.extend({
+      name: options.name,
+      requiredConfig: [
+        'host',
+        'port'
+      ],
+
+      didBuild: function(context) {
+        //do something amazing here once the project has been built
+      },
+
+      upload: function(context) {
+        //do something here to actually deploy your app somewhere
+      },
+
+      didDeploy: function(context) {
+        //do something here like notify your team on slack
+      }
+    });
+
+    return new DeployPlugin();
+  }
+};
+
 };
